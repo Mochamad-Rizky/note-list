dcc5323b5c36727c52d5b36cf55b954508bd7662
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-12 14:29:12 +0700

    Add Navbar component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,8 +1,10 @@
+import Navbar from "../components/Navbar/Navbar";
+
 const Layout = ({ children }) => {
   return (
     <>
       <header>
-      
+        <Navbar />
       </header>
       <main>
         {children}
