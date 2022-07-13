b2442537ff37e05eec6b25a450e59f1ef8f5a6f7
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-13 14:02:43 +0700

    Add convert date to locale utility

---
+++ src/utils/convertDatetoLocale.js
@@ -0,0 +1,9 @@
+const convertDateToLocale = (date) => {
+  return new Date(date).toLocaleDateString('id-ID', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
+
+export default convertDateToLocale;
