8c468407c49d263f01ee792bd24656214df824ca
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-13 10:04:19 +0700

    Add dummy data

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      title: "Babel",
+      body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+      archived: false,
+      createdAt: '2022-04-14T04:27:34.572Z'
+    },
+    {
+      id: 2,
+      title: "Switch",
+      body: "switch adalah suatu komponen jaringan komputer yang berfungsi untuk menghubungkan beberapa perangkat komputer agar dapat melakukan pertukaran paket, baik menerima, memproses, dan meneruskan data ke perangkat yang dituju.",
+      archived: false,
+      createdAt: '2022-03-14T04:27:34.572Z'
+    },
+    {
+      id: 3,
+      title: "Javascript",
+      body: "Merupakan bahasa pemrograman web. Javascript dapat memanipulasi konten dan atribut elemen yang ada pada website. Javascript juga bisa memodifikasi DOM (Document Object Model) pada web.",
+      archived: false,
+      createdAt: '2022-02-14T04:27:34.572Z'
+    },
+  ];
+}
+
+export default getData;
