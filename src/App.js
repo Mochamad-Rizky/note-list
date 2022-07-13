76b74579f661c68429557983cdc80c7ef43c6bc3
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-13 14:01:21 +0700

    Add note list data functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, {useEffect, useState} from "react";
+
 import Layout from "./layout/Layout";
+import NoteList from "./components/NoteList/NoteList";
+import getData from "./data/data";
 
 import './App.scss';
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+  
+  useEffect(() => {
+    setNotes(getData);
+  }, []);
+  
   return (
     <Layout>
-    
+      <NoteList
+        notes={notes}
+        title="Catatan Aktif"
+        isActive
+      />
     </Layout>
   );
 };
