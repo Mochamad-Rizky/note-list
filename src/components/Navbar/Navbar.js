19dd4ffd6dac8710f4889d9bb0977f354caacacc
Mochamad Rizky Cahya Diputra <moch.rizky.cahya@gmail.com>
2022-07-14 14:26:07 +0700

    Remove anchor home

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,9 +18,6 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
               <a className="nav-link" href="https://www.github.com/Mochamad-Rizky">Github</a>
             </li>
             <li className="nav-item">
